b5fda6bebf7087b1895bb9c54ba2066306887dcd
mukultamrayat <mukultamrayat007@gmail.com>
2025-06-09 12:10:19 +0530

    Second Program

---
+++ src/ex_01_Java_Basic/Lab001/Lab001_HW_Extra.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic.Lab001;
+
+public class Lab001_HW_Extra {
+    public static void main(String[] args) {
+        System.out.println("Hello , This is the second program");
+    }
+}
