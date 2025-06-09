87560eeae37ffad993711e609e8e9f809eb9a74a
mukultamrayat <mukultamrayat007@gmail.com>
2025-06-09 21:56:18 +0530

    Third Program update

--- src/ex_01_Java_Basic/Lab001/Lab003_Extra.java
+++ src/ex_01_Java_Basic/Lab001/Lab003_Extra.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic.Lab001;
 
 public class Lab003_Extra {
     public static void main(String[] args) {
-        System.out.println("GitHub Added Successfully");
+        System.out.println("GitHub Added Successfully update");
     }
 }
