22d07fbcaa8d741e8384e87ae26b7b898cf2b745
mukultamrayat <mukultamrayat007@gmail.com>
2025-06-12 12:47:16 +0530

    Java Basic

---
+++ src/EX_02_Java_Basic_Comments/Lab004_Multiple_Main_Function_inAJava_Program.java
@@ -0,0 +1,11 @@
+package EX_02_Java_Basic_Comments;
+
+public class Lab004_Multiple_Main_Function_inAJava_Program {
+    public static void main(String[] args) {
+        System.out.println("We cannot add multiple main function in java program");
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println("Not Possible");
+//    }
+}
---
+++ src/EX_02_Java_Basic_Comments/Lab005_Add_multiple_function_java_program.java
@@ -0,0 +1,11 @@
+package EX_02_Java_Basic_Comments;
+
+public class Lab005_Add_multiple_function_java_program {
+    public static void main(String[] args) {
+        System.out.println("Yes we can add multi function in java but data type and args are different");
+    }
+
+    public static void main(int args) {
+
+    }
+}
